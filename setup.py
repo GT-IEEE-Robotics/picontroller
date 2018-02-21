2fe38db868664f45a1b40f5a88a01df70cf8d296
hardware-Team <Vi Kumar>
2018-02-21 16:00:41 -0500

    Added setup.py to package with basic information.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='PiController',
+      version='0.1',
+      description='High-level python interface to control robot',
+      url='http://github.com/grokkingStuff/picontroller',
+      author='Vi Kumar',
+      author_email='grokkingStuff@gmail.com',
+      license='',
+      packages=['picontroller'],
+      zip_safe=False)
