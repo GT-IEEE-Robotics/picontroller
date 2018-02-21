6824d1e6feb7ed6b1fdc692d426ed045ddd08246
Vi Kumar <grokkingStuff@gmail.com>
2018-02-21 18:19:28 -0500

    mend

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 
-setup(name='PiController',
+setup(name='gatech-ieee-picontroller',
       version='0.1',
-      description='High-level python interface to control robot',
+      description='High-level python interface to control robot for IEEE competition',
       url='http://github.com/grokkingStuff/picontroller',
       author='Vi Kumar',
       author_email='grokkingStuff@gmail.com',
       license='',
-      packages=['Direction','Filter','Instruction','Robot',],
+      packages=find_packages(),
+      install_requires=['numpy',
+                        'logging',
+                        'warning'],
+      python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, <4',
       zip_safe=False)
